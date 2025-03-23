f816c29579ddc3b028a660df0deff5405d8d1997
Erickson Manuel Holguín <erickson.pena@kiura.co>
2025-03-23 09:03:04 -0400

    Implement API route to retrieve individual blog posts and handle not found cases

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,14 +1,33 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { getCollection, getEntry } from "astro:content";
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
+  const url = URL.parse(request.url)!;
+  const slug = url.searchParams.get("slug");
+
+  if (slug) {
+    const posts = await getEntry("blog", slug as string);
+
+    if (!posts) {
+      return new Response(JSON.stringify({ message: "Post not found", data: null }), {
+        status: 404,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+
+    return new Response(JSON.stringify({ message: "Post retrieved successfully", data: posts }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   const posts = await getCollection("blog");
-
-  const slug = URL.parse(request.url)!;
-
-  console.log(slug.searchParams.get("slug"));
 
   return new Response(JSON.stringify({ message: "Posts retrieved successfully", data: posts }), {
     status: 200,
