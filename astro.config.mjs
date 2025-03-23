32c6f2db62c7eb68fb59767f253ace9dd41ffbbc
Erickson Manuel Holguín <erickson.pena@kiura.co>
2025-03-23 10:26:55 -0400

    Set output to server in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,6 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
+  output: "server",
   adapter: cloudflare(),
 });
